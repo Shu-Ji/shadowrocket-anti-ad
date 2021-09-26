dc8728283505b641efedf640ad31212d2881d95c
Finn <finn@example.com>
2021-09-26 12:59:01 +0800

    update

--- main.py
+++ main.py
@@ -3,13 +3,13 @@
 import re
 from datetime import datetime, timezone, timedelta
 from string import Template
-from urllib import request
+import httpx
 
 tz_utc_8 = timezone(timedelta(hours=8))
 build_time = datetime.now(tz=tz_utc_8).strftime("%Y-%m-%d %H:%M:%S")
 
 url = 'https://anti-ad.net/surge.txt'
-rules = request.urlopen(url).read().decode('u8')
+rules = httpx.get(url).content.decode('u8')
 rules = re.sub(r'DOMAIN-SUFFIX,(.*)', r'DOMAIN-SUFFIX,\1,REJECT', rules)
 
 tmp = Template(open('./sr-template.conf').read())

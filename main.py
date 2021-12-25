006cb7e87c8a6fc68e4d5e1c3fc27137dab4b1f4
Finn <finn@example.com>
2021-12-25 09:24:31 +0800

    使用neodevhost

--- main.py
+++ main.py
@@ -6,13 +6,34 @@ from string import Template
 import httpx
 
 tz_utc_8 = timezone(timedelta(hours=8))
-build_time = datetime.now(tz=tz_utc_8).strftime("%Y-%m-%d %H:%M:%S")
+build_time = datetime.now(tz=tz_utc_8).strftime("%Y-%m-%d")
 
-url = 'https://anti-ad.net/surge.txt'
-rules = httpx.get(url).content.decode('u8')
-rules = re.sub(r'DOMAIN-SUFFIX,(.*)', r'DOMAIN-SUFFIX,\1,REJECT', rules)
+
+def download_from_anti_ad():
+    """https://github.com/privacy-protection-tools/anti-AD"""
+
+    url = 'https://anti-ad.net/surge.txt'
+    rules = httpx.get(url).content.decode('u8')
+    rules = re.sub(r'DOMAIN-SUFFIX,(.*)', r'DOMAIN-SUFFIX,\1,REJECT', rules)
+    return rules
+
+
+def download_from_neodevhost():
+    """https://github.com/neodevpro/neodevhost"""
+
+    res = []
+    url = 'https://neodev.team/lite_domain'
+    rules = httpx.get(url).content.decode('u8').splitlines()
+    for rule in rules:
+        if rule and not rule.startswith('#'):
+            res.append(f'DOMAIN-SUFFIX,{rule},REJECT')
+    return '\n'.join(res)
+
+
+# rules = download_from_anti_ad()
+rules = download_from_neodevhost()
 
 tmp = Template(open('./sr-template.conf').read())
-rules = tmp.substitute(build_time=build_time, anti_ad_rules=rules)
+rules = tmp.substitute(build_time=build_time, rules=rules)
 
 open('./rules/sr-anti-ad.conf', 'w').write(rules)
